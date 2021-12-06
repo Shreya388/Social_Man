f128367a4beb2ec926f148d4b6ad8109d6c74e4d
Shreya388 <meenaxjha@gmail.com>
2021-12-06 16:24:19 +0530

    made Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Container, Col, Row, Card } from "react-bootstrap";
 import TopBar from "./Navbar";
 import d from "./d.png";
+import "./Dashboard.css";
+import Posts from "./pages/Posts";
 
 const Dashboard = () => {
     return (
@@ -10,7 +12,7 @@ const Dashboard = () => {
         <Container>
         <Row>
         <Col md={4}>
-            <div style={{padding: "20pt", marginTop: "20pt", border: "1pt solid #7c7c7c40", background: "white", borderRadius:"6pt", boxShadow: "1pt 1pt 1pt 1pt #e3e2e2"}}>
+            <div className="user">
                 <Container>
                     <h1 className="display-6">Baua</h1>
                     <p className="">Bauax@gmail.com</p>
@@ -18,38 +20,44 @@ const Dashboard = () => {
             </div>
             <Row>
                 <Col>
-                    <div style={{padding: "10pt", marginTop:"10pt", border: "1pt solid #7c7c7c40", background: "white", borderRadius:"6pt", boxShadow: "1pt 1pt 1pt 1pt #e3e2e2"}}>
+                    <div className="views_sec">
                         <h1 className="display-6">5k</h1>
+                        <p className="">Impressions</p>
                     </div>
                 </Col>
                 <Col>
-                <div style={{padding: "10pt", marginTop:"10pt", border: "1pt solid #7c7c7c40", background: "white", borderRadius:"6pt", boxShadow: "1pt 1pt 1pt 1pt #e3e2e2"}}>
-                <h1 className="display-6">160</h1>
+                <div className="views_sec">
+                <h1 className="display-6">170</h1>
+                <p className="">Comments</p>
                 </div>
                 </Col>
             </Row>
 
             <Row>
                 <Col>
-                    <div style={{padding: "10pt", marginTop:"10pt", border: "1pt solid #7c7c7c40", background: "white", borderRadius:"6pt", boxShadow: "1pt 1pt 1pt 1pt #e3e2e2"}}>
+                    <div className="views_sec">
                         <h1 className="display-6">120</h1>
+                        <p className="">Likes</p>
                     </div>
                 </Col>
                 <Col>
-                <div style={{padding: "10pt", marginTop:"10pt", border: "1pt solid #7c7c7c40", background: "white", borderRadius:"6pt", boxShadow: "1pt 1pt 1pt 1pt #e3e2e2"}}>
+                <div className="views_sec">
                 <h1 className="display-6">50</h1>
+                <p className="">Mentions</p>
                 </div>
                 </Col>
             </Row>
         </Col>
         <Col>
-            <Card style={{ marginTop: "20pt", border: "1pt solid #7c7c7c40", boxShadow: "1pt 1pt 1pt 1pt #e3e2e2", borderRadius:"6pt"}}>
-                <img src={d} alt="stats" style={{}} />    
+            <Card className="card_sec" >
+                <img src={d} alt="stats" />    
             </Card>
 
         </Col>
         </Row>
         </Container>
+
+        
         </>
      );
 }
---
+++ src/pages/Posts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Posts = () => {
+    return ( 
+        <div className="posts">
+            <Container>
+            <h1 className="display-3">hello there!</h1>
+            </Container>
+        </div>
+     );
+}
+ 
+export default Posts;
