659f2e04b380c21892dcfe0f406c0b6e05107eee
Shreya388 <meenaxjha@gmail.com>
2021-12-16 00:54:29 +0530

    made messsages page good

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import user from "./user.png";
+import "./Messages.css";
 
 const Messages = () => {
     return ( 
@@ -19,18 +21,22 @@ const Messages = () => {
 
             <Row style={{background: "white", height: "470pt", textAlign: "left"}}>
                 <Col sm={4} style={{border: "1pt solid #dddddd", padding:"0"}}>
-                    <div style={{background: "#eeeeee", padding: "10pt 30pt"}}>
-                        <p className="navbar-brand">Sneha</p>
-                        <p className="">loren ipsum dolor sit amet</p>
+
+                    <input className="chatInput" type="text" placeholder="Search Chat" />
+
+                    <div style={{background: "rgb(233 233 233 / 76%)", padding: "5pt 30pt", borderBottom: "1pt solid #bdbdbdad"}}>
+                    
+                        <p className="navbar-brand"><img src={user} alt="us" height="40pt" /> Sneha</p>
+                        <p className="">Hi, what's up</p>
                     </div>
-                    <div style={{background: "white", padding: "10pt 30pt"}}>
-                        <p className="navbar-brand">User</p>
-                        <p className="">loren ipsum dolor sit amet</p>
+                    <div style={{background: "white", padding: "5pt 30pt"}}>
+                        <p className="navbar-brand"><img src={user} alt="us" height="40pt" />User</p>
+                        <p className="">Good Morning! how are you?</p>
                     </div>        
                 </Col>
                 <Col sm={8} style={{border: "1pt solid #dddddd"}}>
-                <div>
-                        
+                    <div style={{ marginTop:"420pt"}}>
+                        <input type="text" style={{border: "1pt solid silver", borderRadius: "7pt", background: "rgba(233, 233, 233, 0.76)", padding:"6pt 10pt", width: "500pt"}}/>
                     </div>  
                 </Col>
             </Row>
